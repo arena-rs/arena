1a13412f15c44eb01e8c85880c44805bcb9365a4
ts0yu <120932697+ts0yu@users.noreply.github.com>
2024-08-09 15:47:52 +0000

    feat(refactor): strategy, feed, arena, config

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,24 @@
-pub mod strategy;
+pub mod arena;
+pub mod config;
 pub mod feed;
+pub mod strategy;
 
-use crate::feed::Feed;
+use alloy::{
+    network::{Ethereum, EthereumWallet},
+    node_bindings::{Anvil, AnvilInstance},
+    providers::{
+        fillers::{ChainIdFiller, FillProvider, GasFiller, JoinFill, NonceFiller, WalletFiller},
+        Identity, RootProvider,
+    },
+    transports::http::{Client, Http},
+};
+
+pub type AnvilProvider = FillProvider<
+    JoinFill<
+        JoinFill<JoinFill<JoinFill<Identity, GasFiller>, NonceFiller>, ChainIdFiller>,
+        WalletFiller<EthereumWallet>,
+    >,
+    RootProvider<Http<Client>>,
+    Http<Client>,
+    Ethereum,
+>;
